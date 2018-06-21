5cffe641ca49566d9157a69e2080900a35fa7d0c
Michael Lämmlein <ml@spacerabbit.de>
2018-06-21 22:23:27 +0200

    frontend to set user and password for vimeo login

--- info.php
+++ info.php
@@ -3,19 +3,30 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $session = new Laemmi\YoutubeDownload\Session();
 
-$url = isset($_POST['url']) ? $_POST['url'] : null;
+$url      = isset($_POST['url'])      ? filter_var($_POST['url'], FILTER_SANITIZE_STRING)      : null;
+$user     = isset($_POST['user'])     ? filter_var($_POST['user'], FILTER_SANITIZE_STRING)     : null;
+$password = isset($_POST['password']) ? filter_var($_POST['password'], FILTER_SANITIZE_STRING) : null;
 
-$error = false;
+$error = '';
 try {
-    $service = Laemmi\YoutubeDownload\Service::factory($url);
+    $options = null;
+    if ($user && $password) {
+        $options = new \Laemmi\YoutubeDownload\Service\VimeoOptions();
+        $options->setAuthenticate(true);
+        $options->setAuthenticateCredentials([
+            'email'    => $user,
+            'password' => $password,
+        ]);
+    }
+    $service = Laemmi\YoutubeDownload\Service::factory($url, $options);
     $session->data = $service->getData();
 } catch (Exception $e) {
-    $error = true;
+    $error = $e->getMessage();
 }
 ?>
 <a href="#" class="btn btn-primary button_new">Andere Video URL eingeben</a>
 <?php if($error): ?>
-    <div class="alert alert-danger" style="margin-top: 20px" role="alert">Ups... Du hast eine ungültige Video Url eingegeben </div>
+    <div class="alert alert-danger" style="margin-top: 20px" role="alert">Ups... Du hast eine ungültige Video Url eingegeben (<?php echo $error; ?>)</div>
 <?php else: ?>
     <div class="media">
         <div class="media-left">
